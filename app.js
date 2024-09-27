507c302c1e767cc9d7dc2f2c91843d8d5eb8c484
Jethanandj52 <134337163+Jethanandj52@users.noreply.github.com>
2024-09-27 07:16:34 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,78 @@
+//Quesiton 1
+// var stundentName=[];
+
+// document.write(stundentName)
+
+//Question 2
+// var stundentName= stundentFullName();
+// document.write(stundentName)
+
+//Question 3
+
+// var studentName=["Jethannad","Sarwan","Waqar"]
+
+// alert("Question no:3\n"+studentName)
+
+//Question 4
+// var studentAge=[20,21,23]
+
+// alert("Question no:4\n"+studentAge)
+
+//Question 5
+
+// var boolean=[true, false]
+
+// alert("Question no:5\n"+boolean)
+
+//Question 6
+
+// var mixArray=["Jethannad ","Sarwan ","Waqar ",20, 21, 23, true, false]
+
+// alert("Question no:6\n"+mixArray)
+
+//Question 7
+
+// var qualification=["SSC ","HSC","BSC","BS","BCOM","MS","M.Phil","PhD"]
+// document.write("Question no: 7 <br>")
+// for(var i=0;i<=qualification.length;i++){
+//     document.write(i+1 + " "+qualification[i] + "<br>");
+// }
+
+// Question 8
+// var studentName=["Jethanand","Waqar"," Sarwam",]
+// var marks=[320,230,480]
+// var totalMarks=500
+// document.write("Question no: 8 <br>")
+// for(var i=0;i<=studentName.length;i++){
+    
+//     var percentage= (marks[i]/totalMarks) *100
+//     document.write("Score of : "+studentName[i] + "is"+marks[i]+"Precentage:"+percentage+"<br>")
+    
+// }
+
+// Question 9
+
+//Question 10
+ 
+// var numbers=[23,45,22,33,56,9,0,5,2,6]
+
+// document.write("Question no: 10 <br> Scores of Students: "+numbers+"<br>" )
+
+// numbers.sort((a, b) => a - b);
+
+// document.write("Order Score: "+numbers+"<br>")
+
+//Question no:11
+
+// var citiesName=["Karachi","Lahore","Islamabad","Quetta","Peshawar"]
+// document.write("Question no:11 <br>Cities: "+citiesName+"<br>")
+
+// document.write("<br> Selected Cities: "+citiesName[0]+" "+citiesName[1]+" "+citiesName[2])
+
+//Question no:12
+
+// var array=["This ","is ","my ","cat "]
+
+// var newArray=array.join('')
+// console.log(newArray);
+
